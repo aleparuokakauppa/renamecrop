3754bd3944f06df00a42605c9cfb5cc053781f5d
aleparuokakauppa <aleksileinonen2@gmail.com>
2024-02-09 22:03:49 +0200

    UI changes

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ class InternalError(Exception):
 
 # Define the window's contents
 layout = [ 
-    [sg.Text("Select the images to resize and crop", font=('', 30)),
-    sg.FilesBrowse(file_types=(("Image Files", "*.png;*.jpeg;*.jpg;*.webp"),), key="-INPUTFILES-", font=('', 20), target="-COUNT-")],
-    [sg.Text("None selected", font=('', 25), text_color="grey", key="-COUNT-")],
+    [sg.Text("Select the images to resize and crop:", font=('', 25))],
+    [sg.FilesBrowse(file_types=(("Image Files", "*.png;*.jpeg;*.jpg;*.webp"),), key="-INPUT_FILES-", font=('', 20), enable_events=True)],
+    [sg.Listbox(values=[], size=(100, 10), key="-FILE_LISTBOX-")],
 
     # Image basenamess
-    [sg.Text("Give basename for files:", font=('', 30)), sg.InputText(font=('', 30), key="-BASENAME-")],
+    [sg.Text("Give basename for files:", font=('', 30))],
+    [sg.InputText(font=('', 30), key="-BASENAME-")],
 
     # Checkboxes
     [sg.Checkbox("Stretch images? (default: centered)", font=('', 22), key="-IS_STRETCHED-"),
@@ -26,7 +27,8 @@ layout = [
     [sg.Text("New height:", font=('', 25)), sg.Spin(values=[i for i in range(1, 10000)], initial_value=500, key="-HEIGHT-", font=('', 24))],
 
     # Color picker for the background
-    [sg.Text("Background color in RGB. Applicable only in centered-mode.", font=('', 28))],
+    [sg.Text("Background color in RGB.", font=('', 28))],
+    [sg.Text("Used only in centered-mode.", font=('', 18), text_color="grey")],
     [sg.Text("RGB = 255, 255, 255 means white and 0, 0, 0 black.", font=('', 18), text_color="grey")],
     [sg.Text("Red", font=('', 25), text_color="red"), sg.Slider(range=(0, 255), default_value=255, orientation="h", size=(20, 20), pad=((200, 5)), key="-RED-")],
     [sg.Text("Green", font=('', 25), text_color="green"), sg.Slider(range=(0, 255), default_value=255, orientation="h", size=(20, 20), pad=((180, 5)), key="-GREEN-")],
@@ -67,15 +69,15 @@ def resizeImagesCentered(input_files, basename, output_path, target_width, targe
             paste_y = (target_height - new_height) // 2
             resized_image.paste(img.resize((new_width, new_height)), (paste_x, paste_y))
 
+            img_format="jpeg"
             # Check the target format
-            if target_format == False:
-                img_format = "jpeg"
-            else:
+            if target_format == True and img.format != None:
                 img_format = img.format.lower()
                 
             # Save the image with the counter as a suffix
             new_filename = f"{basename}_{counter}.{img_format}"
             resized_image.save(os.path.join(output_path, new_filename))
+
         counter += 1
     return counter
 
@@ -88,10 +90,9 @@ def resizeImagesStretched(input_files, basename, output_path, target_width, targ
             # Resize the image
             resized_image = img.resize((target_width, target_height))
 
+            img_format="jpeg"
             # Check the target format
-            if target_format == False:
-                img_format = "jpeg"
-            else:
+            if target_format == True and img.format != None:
                 img_format = img.format.lower()
 
             # Save the image with the counter as a suffix
@@ -103,16 +104,18 @@ def resizeImagesStretched(input_files, basename, output_path, target_width, targ
 window = sg.Window("Renamecrop", layout, size=(1000, 800))
 
 while True:
+
     event, values = window.read()
 
     if event == 'Quit' or event == sg.WIN_CLOSED:
         break
 
     elif event == 'Run':
-        try:
-            selected_files = []
-            # Define variables
+        # Used to keep track of the selected files
 
+        try:
+            # Used to track the selection of files
+            file_list = []
             base_name = values["-BASENAME-"]
             if base_name == "":
                 raise InternalError("No basename given.")
@@ -136,21 +139,21 @@ while True:
                 # One by one is false, get the new height
                 new_height = int(values["-HEIGHT-"])
 
-            # Append the files from the inputFolder to the selected_files[] list
-            selected_files = values["-INPUTFILES-"].split(";")
+            # Append the files from the -INPUT_FILES- field
+            file_list = values["-INPUT_FILES-"].split(";")
 
             # Filter only image files
-            selected_files = [file for file in selected_files if file.lower().endswith((".png", ".jpg", ".jpeg", ".webp"))]
+            selected_images = [file for file in file_list if file.lower().endswith((".png", ".jpg", ".jpeg", ".webp"))]
 
             # Check if any images were selected
-            if len(selected_files) <= 0:
+            if len(selected_images) <= 0:
                 raise InternalError("No images selected.")
 
             # Execute the resizeImages on all of the images
             if is_stretched == True:
-                resizeImagesStretched(selected_files, base_name, output_folder, new_width, new_height, target_format)
+                resizeImagesStretched(selected_images, base_name, output_folder, new_width, new_height, target_format)
             else:
-                resizeImagesCentered(selected_files, base_name, output_folder, new_width, new_height, target_format, background_color)
+                resizeImagesCentered(selected_images, base_name, output_folder, new_width, new_height, target_format, background_color)
             
             # Success
             sg.popup("Files were resized and renamed")
@@ -158,6 +161,12 @@ while True:
         except InternalError as e:
             # Let the user continue using the program. Don't break the main loop.
             sg.popup_error(e, font=('', 32), title="Non-fatal Error", keep_on_top=True)
+    
+    # Appends the files into the general selected_files variable
+    # And updates the file listbox
+    elif event == "-INPUT_FILES-":
+        file_list = values["-INPUT_FILES-"].split(";")
+        window["-FILE_LISTBOX-"].update(file_list)
 
     elif event == "-ONEBYONE-":
         window["-HEIGHT-"].update(disabled=values["-ONEBYONE-"])
